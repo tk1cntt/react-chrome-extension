0bad84ca079c5557e89ab2db1d08a524e82bc5b4
Tran Anh Tuan <tk1cntt@gmail.com>
2018-12-19 07:13:33 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Login from "./Login";
 
 class App extends Component {
+  save = (key, value) => {
+    localStorage.setItem(key, value);
+  };
+
   render() {
     return (
       <div className="App">
@@ -12,6 +18,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          <Login save={this.save} />
         </p>
       </div>
     );
--- src/content.js
+++ src/content.js
@@ -1,33 +1,50 @@
 /*global chrome*/
 /* src/content.js */
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Frame, { FrameContextConsumer }from 'react-frame-component';
+import React from "react";
+import ReactDOM from "react-dom";
+import Frame, { FrameContextConsumer } from "react-frame-component";
+import { Form, Icon, Input, Button, Checkbox } from "antd";
 import "./content.css";
+import "./antd.min.css";
+
+import Login from "./Login";
+
+const FormItem = Form.Item;
 
 class Main extends React.Component {
-    render() {
-        return (
-            <Frame head={[<link type="text/css" rel="stylesheet" href={chrome.runtime.getURL("/static/css/content.css")} ></link>]}> 
-               <FrameContextConsumer>
-               {
-               // Callback is invoked with iframe's window and document instances
-                   ({document, window}) => {
-                      // Render Children
-                        return (
-                           <div className={'my-extension'}>
-                               <h1>Hello world - My first Extension</h1>
-                           </div>
-                        )
-                    }
-                }
-                </FrameContextConsumer>
-            </Frame>
-        )
-    }
+  render() {
+    return (
+      <Frame
+        head={[
+          <link
+            type="text/css"
+            rel="stylesheet"
+            href={chrome.runtime.getURL("/static/css/content.css")}
+          />,
+          <link
+            type="text/css"
+            rel="stylesheet"
+            href={chrome.runtime.getURL("/app/antd.min.css")}
+          />
+        ]}
+      >
+        <FrameContextConsumer>
+          {// Callback is invoked with iframe's window and document instances
+          ({ document, window }) => {
+            // Render Children
+            return (
+              <div className={"my-extension"}>
+                <Login save={setItem} />
+              </div>
+            );
+          }}
+        </FrameContextConsumer>
+      </Frame>
+    );
+  }
 }
 
-const app = document.createElement('div');
+const app = document.createElement("div");
 app.id = "my-extension-root";
 
 document.body.appendChild(app);
@@ -35,18 +52,24 @@ ReactDOM.render(<Main />, app);
 
 app.style.display = "none";
 
-chrome.runtime.onMessage.addListener(
-   function(request, sender, sendResponse) {
-      if( request.message === "clicked_browser_action") {
-        toggle();
-      }
-   }
-);
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.message === "clicked_browser_action") {
+    toggle();
+  }
+});
 
-function toggle(){
-   if(app.style.display === "none"){
-     app.style.display = "block";
-   }else{
-     app.style.display = "none";
-   }
+function setItem(key, value) {
+  chrome.storage.sync.set({ [key]: value });
+}
+
+function isAuthentication() {
+  chrome.storage.sync.get(['token']);
+}
+
+function toggle() {
+  if (app.style.display === "none") {
+    app.style.display = "block";
+  } else {
+    app.style.display = "none";
+  }
 }
